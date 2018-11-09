ce3daaf2325945f7690f866ff2e1d6516ab27abc
GLewisBV99 <15664007@students.southwales.ac.uk>
2018-11-09 11:52:34 +0000

    Getting there I think

--- mainGUI.py
+++ mainGUI.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from tkinter import * #GUI framework.
+
+class LoginApplication:
+    def __init__(self, master):
+        pass
+
+if __name__ == "__main__":
+    root = Tk()
+    w = Label(root, text="Hello,world")
+    w.pack()
+    root.mainloop()
