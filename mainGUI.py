df726cd142241f2fbf36e5f63ab69314d6caabcf
glewisBV99 <15664007@students.southwales.ac.uk>
2018-11-23 11:32:30 +0000

    W.I.P

--- mainGUI.py
+++ mainGUI.py
@@ -1,11 +1,44 @@
-from tkinter import * #GUI framework.
+from tkinter import *
+import tkinter.messagebox as tm
 
-class LoginApplication:
+# Totally not copied from stackoverflow or anything...
+
+class LoginFrame(Frame):
     def __init__(self, master):
-        pass
+        super().__init__(master)
 
-if __name__ == "__main__":
-    root = Tk()
-    w = Label(root, text="Hello,world")
-    w.pack()
-    root.mainloop()
+        self.label_username = Label(self, text="Username")
+        self.label_password = Label(self, text="Password")
+
+        self.entry_username = Entry(self)
+        self.entry_password = Entry(self, show="*")
+
+        self.label_username.grid(row=0, sticky=E)
+        self.label_password.grid(row=1, sticky=E)
+        self.entry_username.grid(row=0, column=1)
+        self.entry_password.grid(row=1, column=1)
+
+        self.checkbox = Checkbutton(self, text="Keep me logged in")
+        self.checkbox.grid(columnspan=2)
+
+        self.logbtn = Button(self, text="Login", command=self._login_btn_clicked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def _login_btn_clicked(self):
+        # print("Clicked")
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+
+        # print(username, password)
+
+        if username == "john" and password == "halo":
+            tm.showinfo("Login info", "Welcome John halo")
+        else:
+            tm.showerror("Login error", "Incorrect username")
+
+
+root = Tk()
+lf = LoginFrame(root)
+root.mainloop()
